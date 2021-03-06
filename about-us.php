44b8d8b96a01b371e1de82ead1facbb7ed0f5b2b
Arnab Munshi <arnab47stars@gmail.com>
2021-03-06 10:55:38 +0530

    about-us.php page added

---
+++ about-us.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <!-- navigation start -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="./">Computer Blog</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="about-us.php">About us</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <!-- navigation end -->
+
+    <!-- jumbotron start -->
+    <div class="jumbotron cover-image">
+      <h1 class="display-4 text-primary">About Us</h1>
+    </div>
+    <!-- jumbotron end -->
+
+    <style type="text/css">
+      .cover-image {
+        background-image: url(assets/images/01.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+      }
+    </style>
+
+    <div class="container-fluid">
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+    </div>
+
+    <!-- footer start -->
+    <div class="container-fluid py-5">
+      <div class="row">
+        <div class="col-md-3">
+          <ul>
+            <li><a href="./">Home</a></li>
+            <li><a href="about-us.php">About Us</a></li>
+            <li>Link 3</li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <ul>
+            <li><a href="">Facebook</a></li>
+            <li><a href="https://www.instagram.com/arnabmunshi/" target="blank">Instagram</a></li>
+            <li><a href="">Twitter</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- footer end -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -8,22 +8,20 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 
-    <title>Website</title>
+    <title>Hello, world!</title>
   </head>
   <body>
+    <!-- navigation start -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="./">Computer Blog</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="about-us.php">About us</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -46,7 +44,9 @@
         </form>
       </div>
     </nav>
+    <!-- navigation end -->
 
+    <!-- jumbotron start -->
     <div class="jumbotron">
       <h1 class="display-4">Hello, world!</h1>
       <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
@@ -54,11 +54,13 @@
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </div>
+    <!-- jumbotron end -->
 
+    <!-- cards start -->
     <div class="container-fluid py-5">
       <div class="card-deck">
         <div class="card">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="assets/images/01.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -66,7 +68,7 @@
           </div>
         </div>
         <div class="card">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="assets/images/02.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -74,7 +76,7 @@
           </div>
         </div>
         <div class="card">
-          <img src="..." class="card-img-top" alt="...">
+          <img src="assets/images/03.jpg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -83,7 +85,9 @@
         </div>
       </div>
     </div>
+    <!-- cards end -->
 
+    <!-- slider start -->
     <div class="container-fluid">
       <div id="demo" class="carousel slide" data-ride="carousel">
         <ul class="carousel-indicators">
@@ -93,21 +97,21 @@
         </ul>
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img src="https://www.w3schools.com/bootstrap4/ny.jpg" alt="Los Angeles" width="1100" height="500">
+            <img src="https://www.w3schools.com/bootstrap4/la.jpg" alt="Los Angeles" width="1100" height="500">
             <div class="carousel-caption">
               <h3>Los Angeles</h3>
               <p>We had such a great time in LA!</p>
             </div>   
           </div>
           <div class="carousel-item">
-            <img src="https://www.w3schools.com/bootstrap4/ny.jpg" alt="Chicago" width="1100" height="500">
+            <img src="https://www.w3schools.com/bootstrap4/la.jpg" alt="Chicago" width="1100" height="500">
             <div class="carousel-caption">
               <h3>Chicago</h3>
               <p>Thank you, Chicago!</p>
             </div>   
           </div>
           <div class="carousel-item">
-            <img src="https://www.w3schools.com/bootstrap4/ny.jpg" alt="New York" width="1100" height="500">
+            <img src="https://www.w3schools.com/bootstrap4/la.jpg" alt="New York" width="1100" height="500">
             <div class="carousel-caption">
               <h3>New York</h3>
               <p>We love the Big Apple!</p>
@@ -124,38 +128,48 @@
     </div>
 
     <style>
-    /* Make the image fully responsive */
-    .carousel-inner img {
-      width: 100%;
-      height: 100%;
-    }
+      /* Make the image fully responsive */
+      .carousel-inner img {
+        width: 100%;
+        height: 100%;
+      }
     </style>
+    <!-- slider end -->
 
+    <!-- footer start -->
     <div class="container-fluid py-5">
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-3">
           <ul>
-            <li>link1</li>
-            <li>link2</li>
-            <li>link3</li>
+            <li><a href="./">Home</a></li>
+            <li><a href="about-us.php">About Us</a></li>
+            <li>Link 3</li>
           </ul>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
           <ul>
-            <li>link1</li>
-            <li>link2</li>
-            <li>link3</li>
+            <li><a href="">Facebook</a></li>
+            <li><a href="https://www.instagram.com/arnabmunshi/" target="blank">Instagram</a></li>
+            <li><a href="">Twitter</a></li>
           </ul>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
           <ul>
-            <li>link1</li>
-            <li>link2</li>
-            <li>link3</li>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
           </ul>
         </div>
       </div>
     </div>
+    <!-- footer end -->
 
     <!-- Optional JavaScript; choose one of the two! -->
 
